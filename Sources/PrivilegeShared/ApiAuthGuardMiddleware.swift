3374e9b408e48707627818d264437863f8e34cd0
ChenLin Wang <wangchenlin2001@icloud.com>
2026-08-30 17:34:16 +0800

    V1.0.6.2 为 API 身份认证中间件的 Decoder 设置 ISO8601 日期解析模式

--- Sources/PrivilegeShared/ApiAuthGuardMiddleware.swift
+++ Sources/PrivilegeShared/ApiAuthGuardMiddleware.swift
@@ -5,7 +5,9 @@ public struct ApiAuthGuardMiddleware: AsyncMiddleware {
     public init() {}
     
     public func respond(to request: Request, chainingTo next: any AsyncResponder) async throws -> Response {
-        let data = try JSONDecoder().decode(AuthData.self, from: request.apiAuthData)
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        let data = try decoder.decode(AuthData.self, from: request.apiAuthData)
         request.auth.login(data)
         return try await next.respond(to: request)
     }
