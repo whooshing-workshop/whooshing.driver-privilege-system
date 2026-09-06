afcf1114c6d4836e2ab0df1f35b8cc5be7afa787
ChenLin Wang <wangchenlin2001@icloud.com>
2026-09-06 19:49:23 +0800

    V1.0.7.13 更新依赖

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         .library( name: "PrivilegeModuleDriver", targets: ["PrivilegeModuleDriver"] )
     ],
     dependencies: [
-        .package(url: "https://github.com/whooshing-workshop/whooshing.nexus", branch: "develop"),
-        .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-privilege-system", branch: "develop"),
+        .package(url: "https://github.com/whooshing-workshop/whooshing.nexus", from: "1.0.0"),
+        .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-privilege-system", from: "1.1.1")
     ],
     targets: [
         .target(
